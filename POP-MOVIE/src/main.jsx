6cf315937087b56e2e9a379e9303904eecb40097
nellyNa <nlly.rro@gmail.com>
2023-09-16 00:37:02 -0600

    agregamos BrowserRouter

--- POP-MOVIE/src/main.jsx
+++ POP-MOVIE/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createRoot } from "react-router-dom/client";
-import { createBrowserRouter, RouterProvider, Route, link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, link, BrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter ([
 
@@ -15,6 +15,8 @@ const router = createBrowserRouter ([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
